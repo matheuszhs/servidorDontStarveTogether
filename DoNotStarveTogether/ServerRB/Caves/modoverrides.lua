8ff37f202689fc614d189af1df035435ccc0568c
Ubuntu <ubuntu@ip-172-31-7-76.sa-east-1.compute.internal>
2019-12-06 11:48:52 +0000

    06/12/2018 às 08h48

---
+++ DoNotStarveTogether/ServerRB/Caves/modoverrides.lua
@@ -0,0 +1,30 @@
+
+return {
+  ["workshop-1582864428"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  } 
+}
